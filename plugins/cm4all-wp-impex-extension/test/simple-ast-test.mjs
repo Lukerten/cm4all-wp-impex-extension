b0cc2932fd541d25318a3cfe19c8c515d9bf762f
Lukerten <lc.brendgen@gmail.com>
2022-07-20 13:52:31 +0200

    lastest additions

--- plugins/cm4all-wp-impex-extension/test/simple-ast-test.mjs
+++ plugins/cm4all-wp-impex-extension/test/simple-ast-test.mjs
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert';
 
-test('asttest', (e) => {
+test('simple Ast test', (e) => {
   // Structure of a Simple Site only containing a Header Block
   const source =
     '[{"id":"34a7cb1","elType":"section","settings":[],"elements":[{"id":"712bf1a","elType":"column","settings":{"_column_size":100,"_inline_size":null},"elements":[{"id":"d6630b2","elType":"widget","settings":{"title":"Hello World"},"elements":[],"widgetType":"heading"}],"isInner":false}],"isInner":false}]';
